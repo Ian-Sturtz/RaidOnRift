0607925c9de5e6211bc289a3aa6dc0b99dfedeba
SlattengrenGarrett <glslattengren@nevada.unr.edu>
2024-01-26 23:01:00 -0800

    Piece Selection UI Framework

---
+++ Assets/Scripts/PieceSelection.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PieceSelection : MonoBehaviour
+{
+    public GameObject Piece;
+    private int royal1 = 0;
+    private int royal2 = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseCount()
+    {
+        
+    }
+}
