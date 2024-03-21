e0e00943a8f76d189ceef7f583caba2c61238176
Ian Sturtz <70338352+Ian-Sturtz@users.noreply.github.com>
2024-03-21 00:17:01 -0700

    Worked on credits

--- Assets/Scripts/UIControllers/CreditsScript.cs
+++ Assets/Scripts/UIControllers/CreditsScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class CreditsScript : MonoBehaviour
@@ -11,6 +12,10 @@ public class CreditsScript : MonoBehaviour
     [SerializeField] private int sceneCount = 1;
     [SerializeField] private GameObject AudioSource;
     [SerializeField] private float audioLength;
+    [SerializeField] private int currentScene = 0;
+    [SerializeField] private string[] credits = new string[10];
+    [SerializeField] private TMP_Text creditText;
+    [SerializeField] private float elapsedTime;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +23,7 @@ public class CreditsScript : MonoBehaviour
         audioLength = AudioSource.GetComponent<AudioSource>().clip.length;
         creditDuration = audioLength;
         sceneDuration = creditDuration / sceneCount;
-        //StartCoroutine(RollCredits());
+        StartCoroutine(RollCredits());
     }
 
     private void Update()
@@ -30,21 +35,42 @@ public class CreditsScript : MonoBehaviour
         }
     }
 
-    //IEnumerator RollCredits()
-    //{
-    //    float startTime = Time.time;
-    //    float elapsedTime;
-    //    float sceneTime;
-    //    float sceneStart;
+    private void nextScene()
+    {
+        if (currentScene < sceneCount)
+        {
+            currentScene += 1;
 
-    //    while (creditDuration > 0)
-    //    {
-    //        elapsedTime = Time.time - startTime;
-    //        creditDuration -= elapsedTime;
+            creditText.SetText(credits[currentScene - 1]);
+        }
+    }
+
+    IEnumerator RollCredits()
+    {
+        float startTime = Time.time;
+        float sceneTime = 0f;
+        nextScene();
+
+        while (creditDuration > 0)
+        {
+            elapsedTime = 0f;
+            elapsedTime = Time.time - startTime;
+            startTime = elapsedTime;
+            creditDuration = audioLength - elapsedTime;
+            sceneTime += elapsedTime;
 
             
-    //    }
+            // Scene has not gone on for long enough
+            if(sceneTime < sceneDuration)
+            {
+                yield return new WaitForSeconds(1f);
+            }
+            else
+            {
+                nextScene();
+            }
+        }
 
-    //    SceneManager.LoadScene("Main Menu");
-    //}
+        SceneManager.LoadScene("Main Menu");
+    }
 }
