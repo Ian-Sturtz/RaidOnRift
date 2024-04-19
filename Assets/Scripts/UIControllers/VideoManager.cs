15bfaa12e3ce4cc7c1db872e0b5a2825323cbbed
SlattengrenGarrett <glslattengren@nevada.unr.edu>
2024-04-18 21:07:15 -0700

    Added remaining videos

--- Assets/Scripts/UIControllers/VideoManager.cs
+++ Assets/Scripts/UIControllers/VideoManager.cs
@@ -6,20 +6,33 @@ public class VideoManager : MonoBehaviour
 {
     [SerializeField] private VideoClip navyRoyal1;
     [SerializeField] private VideoClip navyRoyal2;
+    [SerializeField] private VideoClip navyQuartermaster;
+    [SerializeField] private VideoClip navyCannon;
+    [SerializeField] private VideoClip navyEngineer;
+    [SerializeField] private VideoClip navyVanguard;
+    [SerializeField] private VideoClip navyNavigator;
+    [SerializeField] private VideoClip navyGunner;
+    [SerializeField] private VideoClip navyMate;
 
     [SerializeField] private VideoClip pirateRoyal1;
     [SerializeField] private VideoClip pirateRoyal2;
-
-    [SerializeField] public VideoClip quartermaster;
-    [SerializeField] public VideoClip cannon;
-    [SerializeField] public VideoClip engineer;
-    [SerializeField] public VideoClip vanguard;
-    [SerializeField] public VideoClip navigator;
-    [SerializeField] public VideoClip gunner;
-    [SerializeField] public VideoClip mate;
+    [SerializeField] private VideoClip pirateQuartermaster;
+    [SerializeField] private VideoClip pirateCannon;
+    [SerializeField] private VideoClip pirateEngineer;
+    [SerializeField] private VideoClip pirateVanguard;
+    [SerializeField] private VideoClip pirateNavigator;
+    [SerializeField] private VideoClip pirateGunner;
+    [SerializeField] private VideoClip pirateMate;
 
     [HideInInspector] public VideoClip royal1;
     [HideInInspector] public VideoClip royal2;
+    [HideInInspector] public VideoClip quartermaster;
+    [HideInInspector] public VideoClip cannon;
+    [HideInInspector] public VideoClip engineer;
+    [HideInInspector] public VideoClip vanguard;
+    [HideInInspector] public VideoClip navigator;
+    [HideInInspector] public VideoClip gunner;
+    [HideInInspector] public VideoClip mate;
 
     public void Setup(bool isNavy)
     {
@@ -27,11 +40,25 @@ public class VideoManager : MonoBehaviour
         {
             royal1 = navyRoyal1;
             royal2 = navyRoyal2;
+            quartermaster = navyQuartermaster;
+            cannon = navyCannon;
+            engineer = navyEngineer;
+            vanguard = navyVanguard;
+            navigator = navyNavigator;
+            gunner = navyGunner;
+            mate = navyMate;
         }
         else
         {
             royal1 = pirateRoyal1;
             royal2 = pirateRoyal2;
+            quartermaster = pirateQuartermaster;
+            cannon = pirateCannon;
+            engineer = pirateEngineer;
+            vanguard = pirateVanguard;
+            navigator = pirateNavigator;
+            gunner = pirateGunner;
+            mate = pirateMate;
         }
     }
 }
