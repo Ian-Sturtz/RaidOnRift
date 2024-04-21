e3e574f956855e24f551c9d67dd463a4dbf4976a
Pettersen-Ian <70539355+Pettersen-Ian@users.noreply.github.com>
2024-04-21 00:04:10 -0700

    Update DisplayIP.cs

--- Assets/Scripts/Networking/DisplayIP.cs
+++ Assets/Scripts/Networking/DisplayIP.cs
@@ -1,30 +1,41 @@
 using UnityEngine;
-using UnityEngine.UI;  // For UGUI
-using TMPro;  // For TextMeshPro
-using System.Net;
+using TMPro; 
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Linq;
 
 public class DisplayIP : MonoBehaviour
 {
-    public TextMeshProUGUI ipTextTMP;  
-    
+    public TextMeshProUGUI ipTextTMP;
 
     void Start()
     {
-        ipTextTMP.text = "Your IP Address: " + GetLocalIPAddress();
-        
+        ipTextTMP.text = "Your IP Address: " + GetPreferredIPAddress();
     }
 
-    string GetLocalIPAddress()
+    private string GetPreferredIPAddress()
     {
-        var host = Dns.GetHostEntry(Dns.GetHostName());
-        foreach (var ip in host.AddressList)
+        string ipAddress = "Local IP Address Not Found";
+
+      
+        var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces()
+            .Where(ni => ni.OperationalStatus == OperationalStatus.Up)
+            .Where(ni => ni.GetIPProperties().GatewayAddresses.Any())
+            .Where(ni => ni.GetIPProperties().UnicastAddresses.Any(ua => ua.Address.AddressFamily == AddressFamily.InterNetwork));
+
+        
+        var preferredInterface = networkInterfaces.OrderByDescending(ni => ni.Speed).FirstOrDefault();
+
+        if (preferredInterface != null)
         {
-            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-            {
-                return ip.ToString();
-            }
+        
+            var ipv4Address = preferredInterface.GetIPProperties().UnicastAddresses
+                .FirstOrDefault(ua => ua.Address.AddressFamily == AddressFamily.InterNetwork)?.Address.ToString();
+
+            if (ipv4Address != null)
+                ipAddress = ipv4Address;
         }
-        return "Local IP Address Not Found";
+
+        return ipAddress;
     }
 }
